e1cbba618f8d14d6ec77e0c1547d0d8a71659e2a
Ferhat Kaplan <fkmll2121@gmail.com>
2024-02-08 20:22:59 +0300

    Update the WPM calculation to multiply the word index by 3.

--- src/components/ResultCard.jsx
+++ src/components/ResultCard.jsx
@@ -7,7 +7,7 @@ function ResultCard() {
       <div className="pt-10 w-60 flex flex-col ">
         <div className="bg-green-700 text-white px-4 font-bold">Result</div>
         <div className="text-4xl font-bold text-green-700 text-center p-4 odd:bg-white even:bg-slate-100">
-          {wordIndex} WPM
+          {wordIndex * 3} WPM
         </div>
         <div className="flex justify-between px-4 odd:bg-white even:bg-slate-100">
           <span className="text-sm">Accuracy</span>{" "}
